c2e73d1da8c86875edcfb2d66273072670250195
Rhys <rhyswoody@gmail.com>
2022-02-23 14:14:40 -0800

    add plurality function

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,14 +4,25 @@ import classNames from "classnames";
 
 
 export default function DayListItem(props) {
-  let dayClass = classNames("day-list__item", {
+  const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": (props.spots ==0)
   });
+
+  const plurality = spots => {
+    if (spots === 1){
+      return '1 spot remaining'
+    }
+    if (spots === 0){
+      return 'no spots remaining'
+    }
+    return spots + ' spots remaining'
+  }
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{plurality(props.spots)}</h3>
     </li>
   );
 }

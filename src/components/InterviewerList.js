e96d2b62d07bf74d6f23458f01414adda284749d
Rhys <rhyswoody@gmail.com>
2022-02-24 15:03:00 -0800

    refactor interviewerList to Controlled List Naming Conventions

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,13 +5,15 @@ import InterviewerListItem from "./InterviewerListItem";
 export default function InterviewerList(props) {
   console.log(props);
   const listItems = props.interviewers.map((interviewer)=>{
-    return (<InterviewerListItem 
-      key={interviewer.id} 
-      name={interviewer.name} 
-      avatar={interviewer.avatar} 
-      selected={interviewer.id === props.interviewer}
-      setInterviewer={() => props.setInterviewer(interviewer.id)}
-     />)
+    return (
+      <InterviewerListItem 
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === value}
+        setInterviewer={() => onChange(interviewer.id)}    
+      />
+    )
   })
 
   return (

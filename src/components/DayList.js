7b91d2f110f3df78f042cc9da79b5db0f0b07dcd
Rhys <rhyswoody@gmail.com>
2022-02-24 14:32:35 -0800

    add interviewer list array

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,7 +4,13 @@ import DayListItem from "components/DayListItem.js";
 
 export default function DayList(props){
   const listItems = props.days.map((day)=>{
-    return (<DayListItem key={day.id} name={day.name} spots={day.spots} selected={props.days.name === props.day} setDay={props.setDay}  />)
+    return (<DayListItem 
+      key={day.id} 
+      name={day.name} 
+      spots={day.spots} 
+      selected={day.name === props.day} 
+      setDay={props.setDay}
+    />)
   })
     return(
     <ul>

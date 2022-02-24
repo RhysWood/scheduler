db58f984fb75b30cc21b4fa39a2ebe21e824c20a
Rhys <rhyswoody@gmail.com>
2022-02-23 16:09:42 -0800

    fix props.setDay undefined error

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,7 +4,7 @@ import DayListItem from "components/DayListItem.js";
 
 export default function DayList(props){
   const listItems = props.days.map((day)=>{
-    return (<DayListItem day={day} setDay={props.setDay} spots={day.spots} />)
+    return (<DayListItem key={day.id} name={day.name} spots={day.spots} selected={props.days.name === props.day} setDay={props.setDay}  />)
   })
     return(
     <ul>
